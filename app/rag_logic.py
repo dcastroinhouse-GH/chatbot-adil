50cd7c791a43d78c43f7039a0b3f8000b011952b
DAMIAN <dd-damian@Mac-mini-de-DAMIAN.local>
2026-03-24 21:24:51 -0600

    Fix imports in rag_logic.py

--- app/rag_logic.py
+++ app/rag_logic.py
@@ -6,8 +6,9 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 from langchain_community.vectorstores import FAISS
-
-# ...
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
 
 import glob
 
